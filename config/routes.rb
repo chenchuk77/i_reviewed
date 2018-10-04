2ab4175228dc3db543876b0c460a8aac8860c76d
chenchuk <chenchuk@gmail.com>
2018-10-04 22:34:55 +0300

    adding Session resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   # nested notes inside books
   # path to notes will contain also path to books !
   resources :books do
